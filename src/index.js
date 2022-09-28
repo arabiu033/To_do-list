4c582d7971b4246fc3b38cffc61104fae95a0eb2
Aminu Rabiu <arabiu033@gmail.com>
2022-09-28 09:39:09 +0100

    Fix linters

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import _ from 'lodash';
 import './style.css';
 import dots from './images/menu.png';
 import unchecked from './images/unchecked.png';
@@ -8,28 +7,28 @@ const list = [
   { index: 2, desc: 'Drag\'n drop to reorder your list', completed: false },
   { index: 3, desc: 'Manage all your lists in one place', completed: false },
   { index: 4, desc: 'Resync to clear out the old', completed: false },
-]
+];
 
 const ul = document.getElementById('to-do-list');
 const populator = () => {
-  for (let i = 0; i < list.length; i++) {
+  for (let i = 0; i < list.length; i += 1) {
     const li = document.createElement('li');
     li.className = 'priorities';
     const check = document.createElement('img');
     check.src = unchecked;
-  
+
     const p = document.createElement('p');
     p.className = 'desc';
     p.textContent = list[i].desc;
-  
+
     const menu = document.createElement('img');
     menu.className = 'dots';
     menu.style.cursor = 'move';
     menu.src = dots;
-  
+
     li.append(check, p, menu);
     ul.appendChild(li);
   }
-}
+};
 
 populator();
