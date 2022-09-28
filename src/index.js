35e9ba03e11f22b13f0b6aca7d38bda0b78ab4c6
Aminu Rabiu <arabiu033@gmail.com>
2022-09-28 09:24:24 +0100

    Add a simple js file to pupulate the html

--- src/index.js
+++ src/index.js
@@ -1,13 +1,35 @@
 import _ from 'lodash';
 import './style.css';
+import dots from './images/menu.png';
+import unchecked from './images/unchecked.png';
 
- function component() {
-   const element = document.createElement('div');
+const list = [
+  { index: 1, desc: 'Double-tap to edit', completed: false },
+  { index: 2, desc: 'Drag\'n drop to reorder your list', completed: false },
+  { index: 3, desc: 'Manage all your lists in one place', completed: false },
+  { index: 4, desc: 'Resync to clear out the old', completed: false },
+]
 
-  // Lodash, now imported by this script
-   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+const ul = document.getElementById('to-do-list');
+const populator = () => {
+  for (let i = 0; i < list.length; i++) {
+    const li = document.createElement('li');
+    li.className = 'priorities';
+    const check = document.createElement('img');
+    check.src = unchecked;
+  
+    const p = document.createElement('p');
+    p.className = 'desc';
+    p.textContent = list[i].desc;
+  
+    const menu = document.createElement('img');
+    menu.className = 'dots';
+    menu.style.cursor = 'move';
+    menu.src = dots;
+  
+    li.append(check, p, menu);
+    ul.appendChild(li);
+  }
+}
 
-   return element;
- }
-
- document.body.appendChild(component());
+populator();
